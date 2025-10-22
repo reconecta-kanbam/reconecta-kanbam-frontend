eb98185da252583cbce57fa71d7dc8aea56f2622
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-10-22 10:20:42 -0300

    Using the new components

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
 // 1 - Configuração de router
 // import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 /* ============ Pages =========== */
-import KanbanBoard from './components/kanbanBoard/KanbanBoard';
-import AuthPg from './components/login/AuthPg';
-
+import KanbanBoard from "./components/kanbanBoard/KanbanBoard";
+import AuthPg from "./components/login/AuthPg";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectsList from "./components/projects/ProjectsList";
+import Occurrences from "./components/occurrences/Occurrences";
 
 const router = createBrowserRouter([
   {
@@ -25,17 +27,15 @@ const router = createBrowserRouter([
         path: "kanbanBoard",
         element: <KanbanBoard />,
       },
-      //  // 7 - navigate para páginas não existentes
-      // {
-      //   path: "oldcontact",
-      //   element: <Navigate to="/" />,
-      // },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "projects", element: <ProjectsList /> },
+      { path: "occurrences", element: <Occurrences /> },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
