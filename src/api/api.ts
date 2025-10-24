94fba6e388ce53d621e2f984e40e98c8682aef4a
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-10-24 17:26:57 -0300

    fix: api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   withCredentials: false,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
 const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: false,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
 // Interceptor para adicionar token
